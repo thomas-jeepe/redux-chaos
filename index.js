0cc994fe7cef4998b39850d7930b82a8978b6994
penguinsoccer <penguinsoccer@outlook.com>
2015-11-21 14:07:27 -0500

    Exported now -.-

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-function chaos(t,a,e){for(var h,n,o=0;1e5>o;o++)h=t.getState(),n=a[~~(Math.random()*a.length)];t.dispatch(n),e(h,t.getState(),n)||0}
+exports.chaos=function chaos(t,a,e){for(var h,n,o=0;1e5>o;o++)h=t.getState(),n=a[~~(Math.random()*a.length)];t.dispatch(n),e(h,t.getState(),n)||0}
 // so beautiful
